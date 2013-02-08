f0c0f6160fb75e8ad758723defe20b53b58fe7d2
Roy Marmelstein <marmelroy@gmail.com>
2013-02-08 23:20:20 +0000

    Parse setting up + initial setting up

---
+++ Manbag/Manbag/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Manbag
+//
+//  Created by Roy Marmelstein on 08/02/2013.
+//  Copyright (c) 2013 Seedhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
